b338029d07541bac175fd0cdd7023f98959fe4f0
casper <casper.zx@gmail.com>
2012-12-25 02:01:42 +0400

    Depedencies are now built in the deps dir.

--- src/emake.erl
+++ src/emake.erl
@@ -6,14 +6,14 @@
 %%%-------------------------------------------------------------------
 
 -module(emake).
--export([all/0]).
+-export([all/0, clean/0]).
 
 -define(ConfigFile, "Emakefile.em").
 
 -define(AppExt, ".app").
 -define(SrcExt, ".erl").
 
--define(OutDir, "ebin").
+-define(DepsDir, "deps").
 -define(BinDir, "ebin").
 -define(SrcDir, "src").
 -define(IncludeDir, "include").
@@ -24,12 +24,19 @@
 ).
 
 all() ->
-	Deps = read_deps(),
-	IncludePaths = [Dep ++ "/" ++ ?IncludeDir || Dep <- Deps],
-	[build(Dep, ?OutDir, IncludePaths) || Dep <- Deps],
-	[copy_app(Dep, ?OutDir) || Dep <- Deps],
-	build(".", ?OutDir, IncludePaths ++ [?IncludeDir]),
+	DepPaths = read_deps(),
+	IncludePaths = [filename:join(
+		DepPath, ?IncludeDir) || DepPath <- DepPaths],
+	[build(DepPath, filename:join([?DepsDir, filename:basename(DepPath),
+		?BinDir]), IncludePaths) || DepPath <- DepPaths],
+	[copy_app(DepPath, filename:join([?DepsDir,
+		filename:basename(DepPath), ?BinDir])) || DepPath <- DepPaths],
+	build(".", ?BinDir, IncludePaths ++ [?IncludeDir]),
 	ok.
+
+clean() ->
+	os:cmd("rm -r " ++ ?DepsDir),
+	os:cmd("rm " ++ ?BinDir ++ "/*.beam").
 
 read_deps() -> lists:reverse([Dep || {_ID, Dep} <- lists:foldl(
 	fun(Dep = {ID, _}, Deps) -> case lists:keyfind(ID, 1, Deps) of
@@ -38,7 +45,7 @@ read_deps() -> lists:reverse([Dep || {_ID, Dep} <- lists:foldl(
 )]).
 
 read_deps(Path) when is_list(Path) ->
-	read_deps(file:consult(Path ++ "/" ++ ?ConfigFile));
+	read_deps(file:consult(filename:join(Path, ?ConfigFile)));
 read_deps({ok, Config}) -> read_deps(lists:keyfind(deps, 1, Config));
 read_deps({deps, Deps}) -> 
 	[read_deps(Dep) || Dep <- Deps] ++
@@ -47,27 +54,30 @@ read_deps(false) -> []; read_deps({error, enoent}) -> [].
 
 build(Path, OutPath, IncludePaths) ->
 	io:format("Building: ~p~n", [Path]),
-	SrcPath = Path ++ "/" ++ ?SrcDir,
+	SrcPath = filename:join(Path, ?SrcDir),
 	{ok, SrcFiles} = file:list_dir(SrcPath),
-	SrcMods = [SrcPath ++ "/" ++ filename:rootname(File) ||
+	SrcMods = [filename:join(SrcPath, filename:rootname(File)) ||
 		File <- SrcFiles, filename:extension(File) == ?SrcExt],
 	[compile_mod(SrcMod, OutPath, IncludePaths) || SrcMod <- SrcMods].
 
+compile_mod(SrcMod, OutPath, IncludePaths) ->
+	filelib:ensure_dir(OutPath),
+	file:make_dir(OutPath),
+	CompileResult = compile:file(SrcMod,
+		?CompileOptions(OutPath, IncludePaths)),
+	Mod = list_to_atom(filename:basename(SrcMod)),
+	code:purge(Mod), code:load_file(Mod),
+	io:format(" - ~p~n", [CompileResult]).
+
 copy_app(Path, OutPath) ->
 	io:format("Copying app: ~p~n", [Path]),
-	BinPath = Path ++ "/" ++ ?BinDir,
+	BinPath = filename:join(Path, ?BinDir),
 	{ok, BinFiles} = file:list_dir(BinPath),
-	AppFiles = [BinPath ++ "/" ++ File ||
+	AppFiles = [filename:join(BinPath, File) ||
 		File <- BinFiles, filename:extension(File) == ?AppExt],
 	[copy_app_file(AppFile, OutPath) || AppFile <- AppFiles].
 
 copy_app_file(AppFile, OutPath) ->
-	file:copy(AppFile, OutPath ++ "/" ++ filename:basename(AppFile)).
-	
-compile_mod(SrcMod, OutPath, IncludePaths) ->
-	CompileResult = compile:file(SrcMod,
-		?CompileOptions(OutPath, IncludePaths)),
-	Mod = list_to_atom(filename:basename(SrcMod)),
-	code:purge(Mod),
-	code:load_file(Mod),
-	io:format(" - ~p~n", [CompileResult]).
+	filelib:ensure_dir(OutPath),
+	file:make_dir(filename:basename(OutPath)),
+	file:copy(AppFile, filename:join(OutPath, filename:basename(AppFile))).
